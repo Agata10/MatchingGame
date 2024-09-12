a1f14419a640ecaa4339a51a3c558139e1ad3748
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-09-12 17:15:55 -0600

    Add button for modes

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
+let images = JSON.parse(localStorage.getItem('images'));
+let data;
+
 const fetchData = async () => {
-  const response = await fetch(
-    'https://potterapi-fedeperin.vercel.app/en/characters'
-  );
-  const data = await response.json();
-  saveImagesToLocalStorage(data, 1);
-  return data;
+  try {
+    const response = await fetch(
+      'https://potterapi-fedeperin.vercel.app/en/characters'
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error('Fetch error:', err);
+    return [];
+  }
 };
 
 const saveImagesToLocalStorage = (data, mode) => {
   const arrayOfImages = [];
   switch (mode) {
     case 1:
+      for (let i = 0; i < 3; i++) {
+        arrayOfImages.push({ img: data[i].image });
+      }
+      break;
+    case 2:
+      for (let i = 0; i < 4; i++) {
+        arrayOfImages.push({ img: data[i].image });
+      }
+      break;
+    default:
       for (let i = 0; i < 6; i++) {
         arrayOfImages.push({ img: data[i].image });
       }
-      localStorage.setItem('images', JSON.stringify(arrayOfImages));
-      break;
-    case 2:
-      for (let i = 0; i < 8; i++) {
-        arrayOfImages.push({ img: data[i].image });
-      }
-      localStorage.setItem('images', JSON.stringify(arrayOfImages));
-      break;
-    default:
-      for (let i = 0; i < 12; i++) {
-        arrayOfImages.push({ img: data[i].image });
-      }
-      localStorage.setItem('images', JSON.stringify(arrayOfImages));
+
       break;
   }
+  localStorage.setItem('images', JSON.stringify(arrayOfImages));
 };
 
-const cardsWrapper = document.getElementById('wrapper');
-const images = JSON.parse(localStorage.getItem('images'));
+const chooseMode = async (mode) => {
+  if (!images) {
+    data = await fetchData();
+    saveImagesToLocalStorage(data, mode);
+    images = JSON.parse(localStorage.getItem('images'));
+    console.log('images fetched from local storage');
+  }
+  window.location.href = './pages/game.html';
+  const cardsWrapper = document.getElementById('wrapper');
+  cardsWrapper.innerHTML = '';
+  console.log('yes');
 
-if (!images) {
-  fetchData();
-} else {
   images.forEach((image, index) => {
     console.log(image, index);
     const div = document.createElement('div');
@@ -46,4 +58,12 @@ if (!images) {
     div.appendChild(imgHolder);
     cardsWrapper.appendChild(div);
   });
-}
+};
+
+const easyBtn = document.getElementById('easy-mode');
+const mediumBtn = document.getElementById('medium-mode');
+const hardBtn = document.getElementById('hard-mode');
+
+easyBtn.addEventListener('click', () => chooseMode(1));
+mediumBtn.addEventListener('click', () => chooseMode(2));
+hardBtn.addEventListener('click', () => chooseMode(3));
